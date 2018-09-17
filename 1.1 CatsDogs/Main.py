f82dd587bc7cd8cb41acf2e0c695b38730af50ab
lalocho <lalocho13@gmail.com>
2018-09-17 16:03:09 -0700

    Add files via upload

--- 1.1 CatsDogs/Main.py
+++ 1.1 CatsDogs/Main.py
@@ -1,3 +1,6 @@
+#Luis Ochoa
+#80504534
+#mw 10:30-12
 import os
 import random
 
@@ -23,18 +26,39 @@ def process_dir(path):
 
     cat_list = []
     dog_list = []
-    if(dir_list)
 
     # Your code goes here
 
+    # listdir returns the files and folders in a file
+    for each in os.listdir(path):
+        #checking if its a file or another folder
+        if os.path.isfile(os.path.join(path, each)):
+            if ".jpg"  in each:
+                #checks if its a picture file
+                temp = classify_pic(os.path.join(path, each))
+                #sorting based on the number returned by classify_pic
+                if temp >= .5:
+                    dog_list.append(os.path.join(path, each))
+                else:
+                    cat_list.append(os.path.join(path, each))
+        else:
+            #recursively calling if there is a folder in the path
+            temp_list = process_dir(os.path.join(path,each))
+            cat_list.extend(temp_list[0])
+            dog_list.extend(temp_list[1])
     return cat_list, dog_list
+
+
 
 
 def main():
     start_path = './' # current directory
 
-    process_dir(start_path)
+    a,b = process_dir(start_path)
+    for item in a:
+        print (item)
 
-
+    for items in b:
+        print (items)
 main()
 
